6008f5183a95c2ebe3cb6ef006cb5a2c5e06a358
ANN McHEALY <amheal01@gmail.com>
2018-05-07 19:08:44 -0400

    Initial commit of lifemap project

---
+++ ProjectPlan.md
@@ -0,0 +1,14 @@
+left menu bar:
+- clickable life events
+  - what happens?
+
+right content bar:
+  - Map
+  - content
+  - Photo
+  - set of photos and content indexed by location
+
+
+Mobile changes:
+- collapse life events
+- button to reopen them?
---
+++ lifeobject.js
@@ -0,0 +1,94 @@
+// add an event listener for the clicked life event (or jquery?)
+
+const lifeEvents = [
+  {
+    place: 'stCloud',
+    photo: ,
+    content: 'content!',
+  },
+  {
+    place: 'foley',
+    photo: ,
+    content: 'content!',
+  },
+  {
+    place: 'glastonbury',
+    photo: ,
+    content: 'content!',
+  },
+  {
+    place: 'cairo',
+    photo: ,
+    content: 'content!',
+  },
+  {
+    place: 'khartoum',
+    photo: ,
+    content: 'content!',
+  },
+  {
+    place: 'addisAbaba',
+    photo: ,
+    content: 'content!',
+  },
+  {
+    place: 'louisvilleFirst',
+    photo: ,
+    content: ,
+  },
+  {
+    place: 'paris',
+    photo: ,
+    content: ,
+  },
+  {
+    place: 'montreux',
+    photo: ,
+    content: ,
+  },
+  {
+    place: 'florence',
+    photo: ,
+    content: ,
+  },
+  {
+    place: 'rome',
+    photo: ,
+    content: ,
+  },
+  {
+    place: 'nice',
+    photo: ,
+    content: ,
+  },
+  {
+    place: 'montpellier',
+    photo: ,
+    content: ,
+  },
+  {
+    place: 'gainesville',
+    photo: ,
+    content: ,
+  },
+  {
+    place: 'louisvilleSecond',
+    photo: ,
+    content: ,
+  },
+  {
+    place: 'tokyo',
+    photo: ,
+    content: ,
+  },
+  {
+    place: 'southwest',
+    photo: ,
+    content: ,
+  },
+  {
+    place: 'provo',
+    photo: ,
+    content: ,
+  },
+]
